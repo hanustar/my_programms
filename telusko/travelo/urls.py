8e29bd8be9a07fac0f8dbbf7893cbaf0db3502d9
hanustar <hanustar55@gmail.com>
2020-05-06 14:28:35 +0530

    my files of django telusko

--- telusko/travelo/urls.py
+++ telusko/travelo/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from . import views
 urlpatterns = [
-    path('',include("travelo.urls")),
+    path('',views.home, name="home"),
     path('admin/', admin.site.urls)
 ]
